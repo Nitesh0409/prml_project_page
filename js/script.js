b4951057896d01b3d53694a995b297110f1a938e
Nitesh0409 <141953717+Nitesh0409@users.noreply.github.com>
2025-04-09 23:09:50 +0530

    updated script.js
    only web demo link is not working

--- js/script.js
+++ js/script.js
@@ -1,101 +1,97 @@
 document.addEventListener("DOMContentLoaded", () => {
-    project_data = {
-      title: "PRML COURSE PROJECT",
-      heading: "Handwritten Digit Recognition",
-      year: "Pattern Recognition and Machine Learning - 2025",
-      contributers: [
-        {
-          name: "Nitesh",
-          linkedin: "https://www.linkedin.com/in/nitesh-k-kanojia-a839b9309/",
-          profile_pic:
-            "https://media.licdn.com/dms/image/v2/D5603AQHythFmIsaJeQ/profile-displayphoto-shrink_800_800/B56ZTCHnQoHEAc-/0/1738423563795?e=1749686400&v=beta&t=b9-5LkHWkbLF7iZQlL5PhutAapiEEkkbWXh4p8wZSkY",
-          email: "mailto:b23ee1047@iitj.ac.in",
-        },
-        {
-          name: "Nitin Verma",
-          linkedin: "https://www.linkedin.com/in/nitin-verma-2a08b028a/",
-          profile_pic:
-            "https://drive.google.com/thumbnail?id=1aL_BKDV1KJbXoSCaVlSFhTzcwLaGmmg6&sz=s511",
-          email: "mailto:b23ee1048@iitj.ac.in",
-        },
-        {
-          name: "Saurabh Kumar",
-          linkedin:
-            "https://www.linkedin.com/in/saurabh-kumar-07562728a/",
-          profile_pic: "#",
-          email: "mailto:b23ee1066@iitj.ac.in",
-        },
-        {
-          name: "Vanshita Jeenwal",
-          linkedin: "https://www.linkedin.com/in/vanshita-jeenwal-32746a288/",
-          profile_pic:
-            "https://media.licdn.com/dms/image/v2/D4D03AQE5N-06Hjplkw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724336751262?e=1749686400&v=beta&t=YrRXg6FNoTsWcFvyIJZy9NjSX7nkbBNSOS5g8sApn-k",
-          email: "mailto:b23ee1078@iitj.ac.in",
-        },
-        {
-          name: "Shruti Sunil Vibhute",
-          linkedin: "https://www.linkedin.com/in/shruti-vibhute-210b392b6",
-          profile_pic:
-            "https://media.licdn.com/dms/image/v2/D5603AQFTgi9r65WFhw/profile-displayphoto-shrink_400_400/B56ZYUNNqgGcAg-/0/1744095731845?e=1749686400&v=beta&t=_lzyMdBRNYJsss0jpCAgytDlgs3_vIsJUq8bumE6yY4",
-          email: "mailto:b23cs1070@iitj.ac.in",
-        },
-        {
-          name: "Mallam Vishnu Priya",
-          linkedin:
-            "https://www.linkedin.com/in/vishnu-priya-mallam-698a5a2a3/",
-          profile_pic:
-            "https://media.licdn.com/dms/image/v2/D5603AQHh_6iFupI28A/profile-displayphoto-shrink_400_400/B56ZYUBWuyHEAg-/0/1744092622429?e=1749686400&v=beta&t=qt_lJG3ZfKr9ulZOV8tL1s6zvnI0ilE_jOMmzoy1QyU",
-          email: "mailto:b23ee1040@iitj.ac.in",
-        },
-      ],
-      navLinks: [
-        { name: "Web Demo", url: "#" },
-        {
-          name: "Github",
-          url: "https://github.com/NitinVerma2027/PRML-Apr2025",
-        },
-        {
-          name: "Dataset",
-          url: "https://drive.google.com/drive/folders/18h0hDWxSEFYIJUTYN8A-7ZpLEYUlZQ8s?usp=sharing",
-        },
-        { name: "YouTube", url: "#" },
-      ],
-      institute: "India Institute of Technology, Jodhpur",
-      image_pairs: [
-        {
-          drawing_img: `./images/img_2.png`,
-          drawing_caption: `handwritten digit`,
-          prediction_images: `./images/pred_2.png`,
-          prediction_caption: `Predicted digit`,
-        },
-        {
-          drawing_img: `./images/img_6.png`,
-          drawing_caption: `handwritten digit`,
-          prediction_images: `./images/pred_6.png`,
-          prediction_caption: `Predicted digit`,
-        },
-        {
-          drawing_img: `./images/img_5.png`,
-          drawing_caption: `handwritten digit`,
-          prediction_images: `./images/pred_5.png`,
-          prediction_caption: `iPredicted digit`,
-        },
-      ],
-      yt_video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-      exampleImages: [
-        "./example_images/digit_0.png",
-        "./example_images/digit_1.png",
-        "./example_images/digit_2.png",
-        "./example_images/digit_3.png",
-        "./example_images/digit_4.png",
-        "./example_images/digit_5.png",
-        "./example_images/digit_6.png",
-        "./example_images/digit_7.png",
-        "./example_images/digit_8.png",
-      ],
-    };
+  //---------------------------------------------------------------------------------
+  project_data = {
+    title: "PRML COURSE PROJECT",
+    heading: "Handwritten Digit Recognition",
+    year: "Pattern Recognition and Machine Learning - 2025",
+    contributers: [
+      {
+        name: "Nitesh",
+        linkedin: "https://www.linkedin.com/in/nitesh-k-kanojia-a839b9309/",
+        profile_pic: "./profile_pics/nitesh.jpeg",
+        email: "mailto:b23ee1047@iitj.ac.in",
+      },
+      {
+        name: "Nitin Verma",
+        linkedin: "https://www.linkedin.com/in/nitin-verma-2a08b028a/",
+        profile_pic:
+          "https://drive.google.com/thumbnail?id=1aL_BKDV1KJbXoSCaVlSFhTzcwLaGmmg6&sz=s511",
+        email: "mailto:b23ee1048@iitj.ac.in",
+      },
+      {
+        name: "Saurabh Kumar",
+        linkedin: "https://www.linkedin.com/in/saurabh-kumar-07562728a/",
+        profile_pic: "./profile_pics/sourabh.jpg",
+        email: "mailto:b23ee1066@iitj.ac.in",
+      },
+      {
+        name: "Vanshita Jeenwal",
+        linkedin: "https://www.linkedin.com/in/vanshita-jeenwal-32746a288/",
+        profile_pic: "./profile_pics/vanshita.jpeg",
+        email: "mailto:b23ee1078@iitj.ac.in",
+      },
+      {
+        name: "Shruti Sunil Vibhute",
+        linkedin: "https://www.linkedin.com/in/shruti-vibhute-210b392b6",
+        profile_pic: "./profile_pics/shruti.jpeg",
+        email: "mailto:b23cs1070@iitj.ac.in",
+      },
+      {
+        name: "Mallam Vishnu Priya",
+        linkedin: "https://www.linkedin.com/in/vishnu-priya-mallam-698a5a2a3/",
+        profile_pic: "./profile_pics/vishnu_priya.jpeg",
+        email: "mailto:b23ee1040@iitj.ac.in",
+      },
+    ],
+    navLinks: [
+      { name: "Web Demo", url: "#" },
+      {
+        name: "Github",
+        url: "https://github.com/NitinVerma2027/PRML-Apr2025",
+      },
+      {
+        name: "Dataset",
+        url: "https://drive.google.com/drive/folders/18h0hDWxSEFYIJUTYN8A-7ZpLEYUlZQ8s?usp=sharing",
+      },
+      { name: "YouTube", url: "https://youtu.be/GoP_YK_526g" },
+    ],
+    institute: "India Institute of Technology, Jodhpur",
+    image_pairs: [
+      {
+        drawing_img: `./images/img_2.png`,
+        drawing_caption: `handwritten digit`,
+        prediction_images: `./images/pred_2.png`,
+        prediction_caption: `Predicted digit`,
+      },
+      {
+        drawing_img: `./images/img_6.png`,
+        drawing_caption: `handwritten digit`,
+        prediction_images: `./images/pred_6.png`,
+        prediction_caption: `Predicted digit`,
+      },
+      {
+        drawing_img: `./images/img_5.png`,
+        drawing_caption: `handwritten digit`,
+        prediction_images: `./images/pred_5.png`,
+        prediction_caption: `iPredicted digit`,
+      },
+    ],
+    yt_video: "https://youtu.be/GoP_YK_526g",
+    exampleImages: [
+      "./example_images/digit_0.png",
+      "./example_images/digit_1.png",
+      "./example_images/digit_2.png",
+      "./example_images/digit_3.png",
+      "./example_images/digit_4.png",
+      "./example_images/digit_5.png",
+      "./example_images/digit_6.png",
+      "./example_images/digit_7.png",
+      "./example_images/digit_8.png",
+    ],
+  };
 
-    
+  //---------------------------------------------------------------------------------
+
   const heading = document.getElementById("heading");
   heading.textContent = project_data.heading;
 
@@ -114,85 +110,89 @@ document.addEventListener("DOMContentLoaded", () => {
     </div>`;
   });
 
-    const institute_name = document.getElementById('institute_name');
-    institute_name.textContent = project_data.institute;
+  //---------------------------------------------------------------------------------
 
-    const navigation = document.querySelector('.links');
-    navigation.textContent = '';
-    project_data.navLinks.forEach((link, index) => {
-      navigation.innerHTML += `
+  const institute_name = document.getElementById("institute_name");
+  institute_name.textContent = project_data.institute;
+
+  const navigation = document.querySelector(".links");
+  navigation.textContent = "";
+  project_data.navLinks.forEach((link, index) => {
+    navigation.innerHTML += `
     <a href="${link.url}" target="_blank" rel="noopener noreferrer">
       ${link.name}
     </a>
     ${index < project_data.navLinks.length - 1 ? "|" : ""}
   `;
-    });
+  });
 
+  //---------------------------------------------------------------------------------
 
-    const yt_frame = document.querySelector('#frame_yt');
-    yt_frame.src = project_data.yt_video;
+  const yt_frame = document.querySelector("#frame_yt");
+  let yt_url = project_data.yt_video;
 
+  let video_id = yt_url.split("/").pop();
 
-    // --------------------------------------------
+  yt_frame.src = `https://www.youtube.com/embed/${video_id}`;
 
-    let currentIndex = 0;
+  //---------------------------------------------------------------------------------
 
-    const drawImg = document.getElementById("drawing_image");
-    const predImg = document.getElementById("prediction_image");
-    // console.log(drawImg);
+  let currentIndex = 0;
 
-    // Access caption elements correctly
-    const sketchCaption = drawImg.nextElementSibling;
-    const pred_caption = predImg.nextElementSibling;
+  const drawImg = document.getElementById("drawing_image");
+  const predImg = document.getElementById("prediction_image");
 
-    function updateImages(index) {
-      const pair = project_data.image_pairs[index];
-      drawImg.src = pair.drawing_img;
-      predImg.src = pair.prediction_images;
-      sketchCaption.innerText = pair.drawing_caption;
-      pred_caption.innerText = pair.prediction_caption;
-    }
+  const sketchCaption = drawImg.nextElementSibling;
+  const pred_caption = predImg.nextElementSibling;
 
-    document.getElementById("left-btn").addEventListener("click", () => {
-      currentIndex =
-        (currentIndex - 1 + project_data.image_pairs.length) %
-        project_data.image_pairs.length;
-      updateImages(currentIndex);
-    });
+  function updateImages(index) {
+    const pair = project_data.image_pairs[index];
+    drawImg.src = pair.drawing_img;
+    predImg.src = pair.prediction_images;
+    sketchCaption.innerText = pair.drawing_caption;
+    pred_caption.innerText = pair.prediction_caption;
+  }
 
-    document.getElementById("right-btn").addEventListener("click", () => {
-      currentIndex = (currentIndex + 1) % project_data.image_pairs.length;
-      updateImages(currentIndex);
-    });
-
+  document.getElementById("left-btn").addEventListener("click", () => {
+    currentIndex =
+      (currentIndex - 1 + project_data.image_pairs.length) %
+      project_data.image_pairs.length;
     updateImages(currentIndex);
+  });
 
+  document.getElementById("right-btn").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % project_data.image_pairs.length;
+    updateImages(currentIndex);
+  });
 
-    // --------------------------------------------
-    const container = document.querySelector(".example_images");
-    container.innerText = '';
+  updateImages(currentIndex);
 
-    project_data.exampleImages.forEach((src) => {
-      const img = document.createElement("img");
-      img.src = src;
-      img.alt = "Example Image";
-      container.appendChild(img);
-    });
+  //---------------------------------------------------------------------------------
 
-    // -----------------
+  const container = document.querySelector(".example_images");
+  container.innerText = "";
 
-    const teamContainer = document.querySelector(".team_section");
+  project_data.exampleImages.forEach((src) => {
+    const img = document.createElement("img");
+    img.src = src;
+    img.alt = "Example Image";
+    container.appendChild(img);
+  });
 
-    project_data.contributers.forEach((person) => {
-      const card = document.createElement("div");
-      card.classList.add("team_card");
+  //---------------------------------------------------------------------------------
 
-      const profileImage = person.profile_pic;
+  const teamContainer = document.querySelector(".team_section");
 
-      const fallbackImage =
-        "https://cdn-icons-png.flaticon.com/512/149/149071.png"; // Generic user avatar
+  project_data.contributers.forEach((person) => {
+    const card = document.createElement("div");
+    card.classList.add("team_card");
 
-      card.innerHTML = `
+    const profileImage = person.profile_pic;
+
+    const fallbackImage =
+      "https://cdn-icons-png.flaticon.com/512/149/149071.png"; // Generic user avatar
+
+    card.innerHTML = `
     <img src="${profileImage}" alt="${person.name}" class="team_img"
          onerror="this.onerror=null;this.src='${fallbackImage}';" />
     <h3>${person.name}</h3>
@@ -206,8 +206,9 @@ document.addEventListener("DOMContentLoaded", () => {
     </div>
   `;
 
-      teamContainer.appendChild(card);
-    });
-
-
+    teamContainer.appendChild(card);
+  });
 });
+
+  //------------------------------------------😊 Thats All 😊---------------------------------------
+
